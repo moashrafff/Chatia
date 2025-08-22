21eea7e996afd1b2e15fbd4baee3dc1042ff5955
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-08-22 17:07:08 +0300

    init commit

---
+++ composeApp/src/iosMain/kotlin/com/chatia/project/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.chatia.project
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ shared/src/commonMain/kotlin/com/chatia/project/Constants.kt
@@ -0,0 +1,3 @@
+package com.chatia.project
+
+const val SERVER_PORT = 8080
