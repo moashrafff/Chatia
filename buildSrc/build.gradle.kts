5b4c965950b8bd091a57975716dcbb81ea2a4350
mariam <maryamamr2070@gmail.com>
2025-08-25 11:55:42 +0300

    add buildSrc module create packages

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    `kotlin-dsl`
+}
+repositories{
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+dependencies {
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.22.0")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.3")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.51.0")
+    implementation("com.squareup:javapoet:1.13.0")
+    implementation("org.jetbrains.kotlin:kotlin-serialization:2.2.0")
+    implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
+}
